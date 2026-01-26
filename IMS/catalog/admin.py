5e66807421a12848e99a034f5ba0175bbf270c18
Om Kate <work.omkate@gmail.com>
2026-01-26 21:48:05 +0530

    feat: register User model in admin panel

--- IMS/catalog/admin.py
+++ IMS/catalog/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import Component
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import Component, User
 
 # Register your models here.
 admin.site.register(Component)
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ("username", "email", "pid", "role", "is_member")
+    list_filter = ("role", "is_member")
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ("E-Tan Fields", {"fields": ("pid", "role", "phone_number", "is_member")}),
+    )
