f2b2f362ab7a0807826e752ab81ab7362d22fac4
Om Kate <work.omkate@gmail.com>
2026-02-02 12:18:33 +0530

    feat: register Order and OrderItem in admin

--- IMS/catalog/admin.py
+++ IMS/catalog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import Component, User
+from .models import Component, User, Order, OrderItem
 
 # Register your models here.
 admin.site.register(Component)
@@ -13,3 +13,16 @@ class UserAdmin(BaseUserAdmin):
     fieldsets = BaseUserAdmin.fieldsets + (
         ("E-Tan Fields", {"fields": ("pid", "role", "phone_number", "is_member")}),
     )
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "status", "created_at")
+    list_filter = ("status",)
+    search_fields = ("user__username", "user__email")
+    inlines = [OrderItemInline]
